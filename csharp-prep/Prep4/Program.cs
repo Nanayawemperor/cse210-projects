3c150a11c30ffc5d520d8af0569b067d2baa0e6c
EmperorNanayaw <nanayawemperor095@gmail.com>
2024-09-17 05:49:55 +0000

    gh

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<string>words = new List<string>();
+
+        words.Add("phone");
+        words.Add("keyboard");
+        words.Add("mouse");
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+        Console.WriteLine(words.Count);
+
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        string valueFromUser = Console.ReadLine();
+
+        int number = int.Parse(valueFromUser);
+         List<int>numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            string newNumber = Console.ReadLine();
+            int nmber = int.Parse(newNumber);
+            numbers.Add(nmber);
+        }
+        
     }
 }
