8713a864fbc4eb301b209abe422fcdbb57a3eb73
EmperorNanayaw <nanayawemperor095@gmail.com>
2024-09-17 06:09:44 +0000

    working with list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -17,19 +17,49 @@ class Program
         }
         Console.WriteLine(words.Count);
 
-        Console.Write("Enter a list of numbers, type 0 when finished.");
-        string valueFromUser = Console.ReadLine();
 
-        int number = int.Parse(valueFromUser);
-         List<int>numbers = new List<int>();
+        List<int>numbers = new List<int>();
 
-        while (number != 0)
-        {
-            Console.Write("Enter Number: ");
-            string newNumber = Console.ReadLine();
-            int nmber = int.Parse(newNumber);
-            numbers.Add(nmber);
-        }
+        int userNumber = -1;
+
         
+
+        
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+
+            string valueFromUser = Console.ReadLine();
+            userNumber = int.Parse(valueFromUser);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Compute the average 
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
