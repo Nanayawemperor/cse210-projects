c17dd82012a2bbf52a7be3a05c71bbfe31be8568
EmperorNanayaw <nanayawemperor095@gmail.com>
2024-09-26 21:06:37 +0000

    created 3 classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,24 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        Job job2 = new Job();
-        job2._jobTitle = "Software Programmer";
-        job2._company = "Apple";
-        job2._startYear = 2023;
-        job2._endYear = 2024;
-
-        Resume myResume = new Resume();
-        myResume._name = "Emmanuel Okyere";
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
-
+        Console.WriteLine("Hello Sandbox World!!!");
     }
 }
