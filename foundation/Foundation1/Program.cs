7174b58b21cf4b16aa15830735905ab664cefab7
EmperorNanayaw <nanayawemperor095@gmail.com>
2024-10-14 00:23:33 +0000

    Cleared content

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,27 +1,9 @@
 using System;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
-    abstract class video
+    static void Main(string[] args)
     {
-        public abstract void video();
-
-        public void firstVideo()
-        {
-            public string _title;
-            public string _creator;
-            string comment = Console.Write("Thanks for watching. Please leave a comment: ");
-            comment = Console.ReadLine();
-        static void Main(string[] args)
-        {
-            string [] comment = {comment}
-            Console.WriteLine(comment[1]);
-        }
-
-
-        }
+        Console.WriteLine("Hello Foundation1 World!");
     }
-
 }
